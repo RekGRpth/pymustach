86fc726d8f5f24adc3330ff1d11a9cf6d2db2be2
RekGRpth <rekgrpth@gmail.com>
2020-04-29 15:01:08 +0500

    up

--- pymustach.c
+++ pymustach.c
@@ -6,11 +6,10 @@ PyObject *mustach(const char *json, const char *template, const char *file) {
     FILE *out;
     size_t output_len;
     struct json_object *object;
-    text *output;
     if (file) {
-        if (!(out = fopen(file, "wb"))) { PyErr_SetString(PyExc_TypeError, "!fopen"); goto Py_RETURN_NONE; }
+        if (!(out = fopen(file, "wb"))) { PyErr_SetString(PyExc_TypeError, "!fopen"); goto Py_RETURN_NONE_; }
     } else {
-        if (!(out = open_memstream(&output_data, &output_len))) { PyErr_SetString(PyExc_TypeError, "!open_memstream"); goto Py_RETURN_NONE; }
+        if (!(out = open_memstream(&output_data, &output_len))) { PyErr_SetString(PyExc_TypeError, "!open_memstream"); goto Py_RETURN_NONE_; }
     }
     if (!(object = json_tokener_parse(json))) { PyErr_SetString(PyExc_TypeError, "!json_tokener_parse"); goto free; }
     if (fmustach_json_c(template, object, out)) { PyErr_SetString(PyExc_TypeError, "fmustach_json_c"); goto json_object_put; }
@@ -25,6 +24,6 @@ json_object_put:
     if (!json_object_put(object)) { PyErr_SetString(PyExc_TypeError, "!json_object_put"); }
 free:
     if (!file) free(output_data);
-Py_RETURN_NONE:
+Py_RETURN_NONE_:
     Py_RETURN_NONE;
 }

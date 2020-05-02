622a31fe84c465f2c26edbea0aae8c2fd55ae932
RekGRpth <rekgrpth@gmail.com>
2020-05-02 20:10:13 +0500

    up

--- pymustach.c
+++ pymustach.c
@@ -3,6 +3,7 @@
 
 PyObject *mustach(const char *json, const char *template, const char *file) {
     char *output_data;
+    enum json_tokener_error error;
     FILE *out;
     size_t output_len;
     struct json_object *object;
@@ -11,7 +12,7 @@ PyObject *mustach(const char *json, const char *template, const char *file) {
     } else {
         if (!(out = open_memstream(&output_data, &output_len))) { PyErr_SetString(PyExc_TypeError, "!open_memstream"); goto Py_RETURN_NONE_; }
     }
-    if (!(object = json_tokener_parse(json))) { PyErr_SetString(PyExc_TypeError, "!json_tokener_parse"); goto free; }
+    if (!(object = json_tokener_parse_verbose(json, &error))) { PyErr_Format(PyExc_TypeError, "!json_tokener_parse and %s", json_tokener_error_desc(error)); goto free; }
     if (fmustach_json_c(template, object, out)) { PyErr_SetString(PyExc_TypeError, "fmustach_json_c"); goto json_object_put; }
     if (!json_object_put(object)) { PyErr_SetString(PyExc_TypeError, "!json_object_put"); goto free; }
     if (file) Py_RETURN_TRUE; else {

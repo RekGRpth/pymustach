0d28c5cc308ceb1f7264250070f0e35e658f15f6
RekGRpth <rekgrpth@gmail.com>
2021-04-30 14:50:17 +0500

    bytes

--- pymustach.c
+++ pymustach.c
@@ -1,16 +1,20 @@
 #include "pymustach.h"
 #include <mustach/mustach.h>
 
-static PyObject *pymustach_internal(const char *json, const char *template, const char *file, int (*pymustach_process)(const char *template, const char *data, size_t len, FILE *file)) {
-    char *data;
+static PyObject *pymustach_internal(PyObject *json, const char *template, const char *file, int (*pymustach_process)(const char *template, const char *data, size_t len, FILE *file)) {
+    char *input_data;
+    char *output_data;
     FILE *out;
-    size_t len;
+    Py_ssize_t input_len;
+    Py_ssize_t output_len;
+    if (!PyBytes_Check(json)) { PyErr_SetString(PyExc_TypeError, "!PyBytes_Check"); goto ret; }
+    if (PyBytes_AsStringAndSize(json, &input_data, &input_len)) { PyErr_SetString(PyExc_TypeError, "PyBytes_AsStringAndSize"); goto ret; }
     if (file) {
         if (!(out = fopen(file, "wb"))) { PyErr_SetString(PyExc_TypeError, "!fopen"); goto ret; }
     } else {
-        if (!(out = open_memstream(&data, &len))) { PyErr_SetString(PyExc_TypeError, "!open_memstream"); goto ret; }
+        if (!(out = open_memstream(&output_data, (size_t *)&output_len))) { PyErr_SetString(PyExc_TypeError, "!open_memstream"); goto ret; }
     }
-    switch (pymustach_process(template, json, strlen(json), out)) {
+    switch (pymustach_process(template, input_data, input_len, out)) {
         case MUSTACH_OK: break;
         case MUSTACH_ERROR_SYSTEM: PyErr_SetString(PyExc_TypeError, "MUSTACH_ERROR_SYSTEM"); goto free;
         case MUSTACH_ERROR_UNEXPECTED_END: PyErr_SetString(PyExc_TypeError, "MUSTACH_ERROR_UNEXPECTED_END"); goto free;
@@ -27,22 +31,22 @@ static PyObject *pymustach_internal(const char *json, const char *template, cons
     }
     if (file) Py_RETURN_TRUE; else {
         fclose(out);
-        PyObject *bytes = PyBytes_FromStringAndSize(data, (Py_ssize_t)len);
-        free(data);
+        PyObject *bytes = PyBytes_FromStringAndSize(output_data, output_len);
+        free(output_data);
         return bytes;
     }
 free:
-    if (!file) free(data);
+    if (!file) free(output_data);
 ret:
     Py_RETURN_NONE;
 }
 
-PyObject *pymustach(const char *json, const char *template) { return pymustach_internal(json, template, NULL, pymustach_process_json_c); }
-PyObject *pymustach_cjson(const char *json, const char *template) { return pymustach_internal(json, template, NULL, pymustach_process_cjson); }
-PyObject *pymustach_jansson(const char *json, const char *template) { return pymustach_internal(json, template, NULL, pymustach_process_jansson); }
-PyObject *pymustach_json_c(const char *json, const char *template) { return pymustach_internal(json, template, NULL, pymustach_process_json_c); }
+PyObject *pymustach(PyObject *json, const char *template) { return pymustach_internal(json, template, NULL, pymustach_process_json_c); }
+PyObject *pymustach_cjson(PyObject *json, const char *template) { return pymustach_internal(json, template, NULL, pymustach_process_cjson); }
+PyObject *pymustach_jansson(PyObject *json, const char *template) { return pymustach_internal(json, template, NULL, pymustach_process_jansson); }
+PyObject *pymustach_json_c(PyObject *json, const char *template) { return pymustach_internal(json, template, NULL, pymustach_process_json_c); }
 
-PyObject *pymustach_file(const char *json, const char *template, const char *file) { return pymustach_internal(json, template, file, pymustach_process_json_c); }
-PyObject *pymustach_cjson_file(const char *json, const char *template, const char *file) { return pymustach_internal(json, template, file, pymustach_process_cjson); }
-PyObject *pymustach_jansson_file(const char *json, const char *template, const char *file) { return pymustach_internal(json, template, file, pymustach_process_jansson); }
-PyObject *pymustach_json_c_file(const char *json, const char *template, const char *file) { return pymustach_internal(json, template, file, pymustach_process_json_c); }
+PyObject *pymustach_file(PyObject *json, const char *template, const char *file) { return pymustach_internal(json, template, file, pymustach_process_json_c); }
+PyObject *pymustach_cjson_file(PyObject *json, const char *template, const char *file) { return pymustach_internal(json, template, file, pymustach_process_cjson); }
+PyObject *pymustach_jansson_file(PyObject *json, const char *template, const char *file) { return pymustach_internal(json, template, file, pymustach_process_jansson); }
+PyObject *pymustach_json_c_file(PyObject *json, const char *template, const char *file) { return pymustach_internal(json, template, file, pymustach_process_json_c); }
--- pymustach.i
+++ pymustach.i
@@ -1,22 +1,22 @@
 %module pymustach
 %{
-extern PyObject *pymustach(const char *json, const char *template);
-extern PyObject *pymustach_cjson(const char *json, const char *template);
-extern PyObject *pymustach_jansson(const char *json, const char *template);
-extern PyObject *pymustach_json_c(const char *json, const char *template);
+extern PyObject *pymustach(PyObject *json, const char *template);
+extern PyObject *pymustach_cjson(PyObject *json, const char *template);
+extern PyObject *pymustach_jansson(PyObject *json, const char *template);
+extern PyObject *pymustach_json_c(PyObject *json, const char *template);
 
-extern PyObject *pymustach_file(const char *json, const char *template, const char *file);
-extern PyObject *pymustach_cjson_file(const char *json, const char *template, const char *file);
-extern PyObject *pymustach_jansson_file(const char *json, const char *template, const char *file);
-extern PyObject *pymustach_json_c_file(const char *json, const char *template, const char *file);
+extern PyObject *pymustach_file(PyObject *json, const char *template, const char *file);
+extern PyObject *pymustach_cjson_file(PyObject *json, const char *template, const char *file);
+extern PyObject *pymustach_jansson_file(PyObject *json, const char *template, const char *file);
+extern PyObject *pymustach_json_c_file(PyObject *json, const char *template, const char *file);
 %}
 
-extern PyObject *pymustach(const char *json, const char *template);
-extern PyObject *pymustach_cjson(const char *json, const char *template);
-extern PyObject *pymustach_jansson(const char *json, const char *template);
-extern PyObject *pymustach_json_c(const char *json, const char *template);
+extern PyObject *pymustach(PyObject *json, const char *template);
+extern PyObject *pymustach_cjson(PyObject *json, const char *template);
+extern PyObject *pymustach_jansson(PyObject *json, const char *template);
+extern PyObject *pymustach_json_c(PyObject *json, const char *template);
 
-extern PyObject *pymustach_file(const char *json, const char *template, const char *file);
-extern PyObject *pymustach_cjson_file(const char *json, const char *template, const char *file);
-extern PyObject *pymustach_jansson_file(const char *json, const char *template, const char *file);
-extern PyObject *pymustach_json_c_file(const char *json, const char *template, const char *file);
+extern PyObject *pymustach_file(PyObject *json, const char *template, const char *file);
+extern PyObject *pymustach_cjson_file(PyObject *json, const char *template, const char *file);
+extern PyObject *pymustach_jansson_file(PyObject *json, const char *template, const char *file);
+extern PyObject *pymustach_json_c_file(PyObject *json, const char *template, const char *file);

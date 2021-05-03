af91b20d876b6bbf9c376ef2dfe89590992b85ea
RekGRpth <rekgrpth@gmail.com>
2021-05-03 09:09:56 +0500

    up

--- pymustach.h
+++ pymustach.h
@@ -1,3 +1,6 @@
+#ifndef _PYMUSTACH_H
+#define _PYMUSTACH_H
+
 #include <Python.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -5,3 +8,5 @@
 int pymustach_process_cjson(const char *template, size_t length, const char *value, size_t buffer_length, FILE *file);
 int pymustach_process_jansson(const char *template, size_t length, const char *buffer, size_t buflen, FILE *file);
 int pymustach_process_json_c(const char *template, size_t length, const char *str, size_t len, FILE *file);
+
+#endif // _PYMUSTACH_H

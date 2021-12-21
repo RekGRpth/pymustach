7d83359e787ebf7faceb6aed0ba6b5d691cdb138
RekGRpth <rekgrpth@gmail.com>
2021-12-21 10:37:03 +0500

    op

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from distutils.core import setup, Extension
-
-module = Extension(
-    "_pymustach",
-    ["pymustach.i", "pymustach.c", "pymustach-cjson.c", "pymustach-jansson.c", "pymustach-json-c.c"],
-    libraries=["mustach"],
-)
+from distutils.core import Extension, setup
 
 setup(
-    ext_modules=[module],
+    author_email='RekGRpth@gmail.com',
+    author='RekGRpth',
+    ext_modules=[Extension(
+        libraries=["mustach"],
+        name="_pymustach",
+        sources=["pymustach.i", "pymustach.c", "pymustach-cjson.c", "pymustach-jansson.c", "pymustach-json-c.c"],
+    )],
+    license='MIT',
     name="pymustach",
     py_modules=["pymustach"],
     url="https://github.com/RekGRpth/pymustach",
